4bbf079c1bd688d1515d328f19fe912b01f9232d
Timothy Dowling <timothy.dowling@me.com>
2022-07-11 20:24:55 -0400

    dotenv added

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 
 const firebird = require('node-firebird')
+const { db_ip, db_port, db_name, db_user, db_key } = require('./config')
+
--- config.js
+++ config.js
@@ -5,5 +5,7 @@ dotenv.config()
 module.exports = {
     db_ip: process.env.DB_IP,
     db_port: process.env.DB_PORT,
-    masterkey: process.env.MASTERKEY,
+    db_name: process.env.DB_NAME,
+    db_user: process.env.DB_USER,
+    db_key: process.env.DB_KEY,
 }
